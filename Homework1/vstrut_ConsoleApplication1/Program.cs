5597b92f9af923c942f8dff0cf817af88e93de3a
vika4ka <vita.ua.meister@gmail.com>
2017-03-24 12:06:05 +0200

    Adding Homework1

---
+++ Homework1/vstrut_ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ConsoleApplication1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+        public class TestClass
+        {
+        public static JObject Test()
+        {
+            JObject obj = new JObject();
+            return obj;
+        }
+        }
+    }
