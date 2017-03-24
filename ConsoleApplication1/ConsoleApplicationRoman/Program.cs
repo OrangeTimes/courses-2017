4c929578dd7f2bbaff8ee6103f0df3f8a2b6727f
ezart23 <forezart@yahoo.com>
2017-03-24 10:58:23 +0200

    Homework 1

---
+++ ConsoleApplication1/ConsoleApplicationRoman/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq; // this was added to make available JSON object
+
+namespace ConsoleApplicationRoman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class TestClass
+    {
+        public static JObject Test()
+        {
+            JObject obj = new JObject();
+            return obj;
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplicationRoman2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplicationRoman; 
+
+// added reference to previous project - ControlApplicationRoman to make available method Test()
+
+namespace ConsoleApplicationRoman2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calling another assembly");
+            TestClass.Test();
+            Console.WriteLine("Success");
+            Console.ReadKey();
+        }
+    }
+}
