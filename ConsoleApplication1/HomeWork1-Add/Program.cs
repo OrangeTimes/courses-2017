8aab6454c0eec787775e92204500a5320b691abf
denkupets <denis.kupets@gmail.com>
2017-03-24 19:41:31 +0200

    HomeWork1

---
+++ ConsoleApplication1/HomeWork1-Add/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1_Add
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calling another assembly");
+            HomeWork1.TestClass.Test();
+            Console.WriteLine("Success");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication1/HomeWork1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class TestClass
+    {
+       public static JObject Test()
+        {
+            JObject obj = new JObject();
+            return obj;
+        }     
+    }
+
+
+}
