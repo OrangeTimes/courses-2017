1acd5216c797e51cf26b670bf5395e9bb31a3f67
Nazar Kulchytskyi <nazarkin124@gmail.com>
2017-03-24 10:36:59 +0200

    add HW1

---
+++ ConsoleApplication1/ConsoleApplication7/ConsoleApplication7/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChildProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    public class TestClass
+    {
+        public static JObject Test()
+        {
+            JObject obj = new JObject();
+            return obj;
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication7/MainConsoleApplication/Program.cs
@@ -0,0 +1,20 @@
+﻿using ChildProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Calling another assembly");
+            TestClass.Test();
+            Console.WriteLine("Success");
+            Console.ReadKey();
+        }
+    }
+}
