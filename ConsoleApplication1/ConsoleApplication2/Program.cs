5a2807cc2f2a0f5172372c2cc2cb64cfc176dc5d
Yevgen Kramarets <ekramarets@gmail.com>
2017-03-24 10:39:13 +0200

    ykram added hw 1

---
+++ ConsoleApplication1/ConsoleApplication2/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class TestClass
+    {
+        public static JObject Test()
+        {
+            JObject obj = new JObject();
+            return obj;
+        }
+
+    }
+}
+//Need to add refference "using Newtonsoft.Json.Linq;"
+//The "(DateTime.Now)" should be replaced with ()
---
+++ ConsoleApplication1/ConsoleApplication3/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calling another assembly");
+            TestClass.Test();
+            Console.WriteLine("Success");
+            Console.ReadKey();
+        }
+    }
+}
+//Need to add refference "using ConsoleApp1;"
+//Need to add NewtonSoft.Json into app
