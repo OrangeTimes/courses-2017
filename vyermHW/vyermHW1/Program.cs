64508cc9cf400df8cdbdfe60886292b02b2b7ed3
Viktoriia Yermakova <victoria.yermakova@gmail.com>
2017-03-28 11:00:49 +0300

    commited homework

--- vyermHW/vyermHW1/Program.cs
+++ vyermHW/vyermHW1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,14 @@ namespace vyermHW1
     {
         static void Main(string[] args)
         {
+        }
+    }
+    public class TestClass
+    {
+        public static JObject Test()
+        {
+            JObject obj = new JObject();
+            return obj;
         }
     }
 }
---
+++ vyermHW/vyermHW11/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vyermHW1;
+
+namespace vyermHW11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calling another assembly");
+            TestClass.Test();
+            Console.WriteLine("Success");
+            Console.ReadKey();
+        }
+    }
+}
