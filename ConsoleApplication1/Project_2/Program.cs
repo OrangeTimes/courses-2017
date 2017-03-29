091b0741b2b0c8b3e49383e3b535c45e4292158d
NZirchak <hereynadiia@gmail.com>
2017-03-29 19:29:46 +0300

    homework_1

---
+++ ConsoleApplication1/Project_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Project_1;
+using Newtonsoft.Json;
+
+namespace Project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Calling another assembly");
+                TestClass.Test();
+                Console.WriteLine("Success");
+                Console.ReadKey();
+        }
+     }
+}
