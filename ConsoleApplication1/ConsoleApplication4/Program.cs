568e089547ac902ab3db7ea39a5c67a7304ee14a
Yevgen Kramarets <ekramarets@gmail.com>
2017-03-30 10:02:56 +0300

    added training project

---
+++ ConsoleApplication1/ConsoleApplication4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine("What is your foken name?");
+            string myName = Console.ReadLine();
+            Console.Write("Username:" + myName); //concatination
+            //Console.WriteLine(myName);
+            Console.ReadKey();
+            
+
+            /*
+            Console.WriteLine("What is your foken name?");
+            string myName = Console.ReadLine();
+            Console.Write("Username:" + myName); //concatination
+            //Console.WriteLine(myName);
+            Console.ReadKey();
+            */
+
+            /*
+            Console.WriteLine("What is your foken name?");
+            string myName = Console.ReadLine();
+            Console.Write("Username:");
+            Console.WriteLine(myName);
+            Console.ReadKey();
+            */
+
+            /*
+            Console.WriteLine("What is your foken name?");
+            long myName = Convert.ToInt32("hello");
+            // long myName = Convert.ToInt64("832", );
+            Console.Write("Username:" + myName); //concatination
+            Console.ReadKey();
+            */
+
+        }
+    }
+}
