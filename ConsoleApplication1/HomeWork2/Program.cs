b9dc89b2b8430026f1f77067e7c583a6e42bede6
denkupets <denis.kupets@gmail.com>
2017-04-06 19:11:31 +0300

    HomeWork2

--- ConsoleApplication1/HomeWork2/Program.cs
+++ ConsoleApplication1/HomeWork2/Program.cs
@@ -6,11 +6,39 @@ using System.Threading.Tasks;
 
 namespace HomeWork2
 {
-    class Program
+    class CarInfo
     {
         static void Main(string[] args)
         {
-            
+                //Car manufacturer input
+            Console.WriteLine("Manufacturer:");
+            String CarMan = Console.ReadLine();
+            Console.Clear();
+                
+                //Car model input
+            Console.WriteLine("Model:");
+            String CarModel = Console.ReadLine();
+            Console.Clear();
+                
+                //Car year input
+            Console.WriteLine("Year:");
+            String CarYear = Console.ReadLine();
+            Console.Clear();
+                
+                //Car price input
+            Console.WriteLine("Price: ($)");
+            String CarPrice = Console.ReadLine();
+            Console.Clear();
+                
+                //Describing the issue
+            Console.WriteLine("Please describe a problem:");
+            String TechSubmission = Console.ReadLine();
+            Console.Clear();
+                
+                //Showing the results
+            Console.Write("Manufacturer: " + CarMan + " | " + "Model: " + CarModel + " | " + "Year: " + CarYear + " | " + "Price: " + CarPrice + " $ " + " | " + "Issue: " + TechSubmission);
+            Console.ReadKey();
         }
+
     }
 }
