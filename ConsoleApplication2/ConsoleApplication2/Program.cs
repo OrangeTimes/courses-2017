4843002921e8da72f0f5acc8a2c299b0336bb352
vika4ka <vita.ua.meister@gmail.com>
2017-04-07 08:37:31 +0300

    Added Homework 2

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please specify the manufacturer of the car you'd like to buy:");
+            string carManufacturer = Console.ReadLine();
+            Console.Write("Please select the model of the car: ");
+            string carModel = Console.ReadLine();
+            Console.Write("Please specify the year of production: ");
+            int carYearOfProduction = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Specify the maximum suitable price in dollars: ");
+            int carMaxPrice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please confirm the following information is correct. You would like to buy " + carManufacturer + " " + carModel + ", of " + carYearOfProduction + " year of production, for " + carMaxPrice + "$.");
+            Console.ReadKey();
+        }
+    }
+}
