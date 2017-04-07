7645b71eafee79ec705cf4249e9e680b40ad1ccd
Nazar Kulchytskyi <nazarkin124@gmail.com>
2017-04-07 14:03:42 +0300

    Move HW3 to proper folder

--- NazarK/ConsoleApplicationOperators/Program.cs
+++ NazarK/ConsoleApplicationOperators/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleHW3
+namespace ConsoleApplicationOperators
 {
     class Program
     {
--- NazarK/ConsoleApplicationOperators/Properties/AssemblyInfo.cs
+++ NazarK/ConsoleApplicationOperators/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleHW3")]
+[assembly: AssemblyTitle("ConsoleApplicationOperators")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleHW3")]
+[assembly: AssemblyProduct("ConsoleApplicationOperators")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ae5508b1-96b1-4765-b14a-29d48be64014")]
+[assembly: Guid("41f3e9c9-75c9-4c10-98d1-9b91b882c38c")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
