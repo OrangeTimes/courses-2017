53e4adbaf9d3cae671a49f911cd025ea2de48840
Yevgen Kramarets <ekramarets@gmail.com>
2017-04-07 22:07:57 +0300

    added HW1

---
+++ ConsoleApplication1/ParentProject/StartUpProject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ChildProject;
+
+namespace StartUpProject
+{
+    class StartUpProject
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calling another assembly");
+            TestClass.Test();
+            Console.WriteLine("Success");
+            Console.ReadKey();
+        }
+    }
+}
+//Need to add refference "using ConsoleApp1;"
+//Need to add NewtonSoft.Json into app
---
+++ ConsoleApplication1/childProject/ChildProject.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace ChildProject
+{
+    class ChildProject
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class TestClass
+    {
+        public static JObject Test()
+        {
+            JObject obj = new JObject();
+            return obj;
+        }
+
+    }
+}
+//Need to add refference "using Newtonsoft.Json.Linq;"
+//The "(DateTime.Now)" should be replaced with ()
