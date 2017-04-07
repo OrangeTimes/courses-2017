24ebfe94daf1a834eec208fbe7c51dc0ed30ffbc
vtaben <vovataben@gmail.com>
2017-04-07 10:25:53 +0300

    change 1

---
+++ ConsoleApplication1/HomeWork1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+        }
+    }
+}
