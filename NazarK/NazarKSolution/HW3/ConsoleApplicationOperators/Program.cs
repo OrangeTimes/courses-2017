4e023754db6333f1c9ffb0ad69effe7c0f4624d7
Nazar Kulchytskyi <nazarkin124@gmail.com>
2017-04-08 18:19:49 +0300

    Add HW3

--- NazarK/NazarKSolution/HW3/ConsoleApplicationOperators/Program.cs
+++ NazarK/NazarKSolution/HW3/ConsoleApplicationOperators/Program.cs
@@ -10,6 +10,22 @@ namespace ConsoleApplicationOperators
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine(-1 + 3 * 5);
+            //Console.WriteLine((24 + 5) % 7);
+            //Console.WriteLine(15 + -4 * 6 / 11);
+            //Console.WriteLine(2 + 10 / 6 * 1 - 7 % 2);
+
+            int firstNumber;
+            int secondNumber;
+            Console.Write("Please enter first number: ");
+            firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter second number (bigger than 0): ");
+            secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+            Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+            Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+            Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
+            Console.WriteLine($"{firstNumber} % {secondNumber} = {firstNumber % secondNumber}");
         }
     }
 }
