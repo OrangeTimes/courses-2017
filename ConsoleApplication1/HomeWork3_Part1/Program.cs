c2cc3ac303115ff3b9b418a6eca8f5d1b11c6f80
denkupets <denis.kupets@gmail.com>
2017-04-13 19:09:44 +0300

    HomeWork3 - Denys

---
+++ ConsoleApplication1/HomeWork3_Part1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3_Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = -1 + 3 * 5;
+            int b = (24 + 5) % 7;
+            int c = 15 + -4 * 6 / 11;
+            int d = 2 + 10 / 6 * 1 - 7 % 2;
+
+            Console.WriteLine("Here are the outcomes: a = {0}, b = {1}, c = {2}, d = {3}", a, b, c, d);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApplication1/HomeWork3_Part2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3_Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int num1;
+            int num2;
+            int num3;
+            int num4;
+
+            //Adding numbers typed by the user.
+
+            Console.WriteLine("Let's do some adding:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("and");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            num1 = a + b;
+            Console.WriteLine(a + " + " + b + " = " + "{0}", num1);
+            Console.WriteLine("U got your numbers. Now press enter to go next");
+            Console.ReadLine();
+            Console.Clear();
+
+            //Subtracting numbers typed by the user.
+            Console.WriteLine("Let's do some subtracting:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("and");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            num2 = a - b;
+            Console.WriteLine(a + " - " + b + " = " + "{0}", num2);
+            Console.WriteLine("U got your numbers. Now press enter to go next");
+            Console.ReadLine();
+            Console.Clear();
+
+            //Multiplying  numbers typed by the user.
+            Console.WriteLine("Let's do some multiplying:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("and");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            num3 = a * b;
+            Console.WriteLine(a + " * " + b + " = " + "{0}", num3);
+            Console.WriteLine("U got your numbers. Now press enter to go next");
+            Console.ReadLine();
+            Console.Clear();
+
+            //Dividing numbers typed by the user.
+            Console.WriteLine("Let's do some dividing:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("and");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            num4 = a / b;
+            Console.WriteLine(a + " / " + b + " = " + "{0}", num4);
+            Console.WriteLine("U got your numbers. Now press enter to go next");
+            Console.ReadLine();
+            Console.Clear();
+
+            //Displaying all nums
+            Console.WriteLine("Your results from all previous equations: {0}, {1}, {2}, {3}", num1, num2, num3, num4);
+            Console.ReadLine();
+        }
+    }
+}
