451bdecd530e60a3f0a07ad31a15876f155c7780
Yevgen Kramarets <ekramarets@gmail.com>
2017-04-13 22:24:35 +0300

    Finished with HW3

---
+++ ConsoleApplication1/HW3/CalculationApp/CalculationApp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CalculationApp
+{
+    class CalculationApp
+    {
+        static void Main(string[] args)
+        {
+            int UserNumberOne, UserNumberTwo;
+
+            Console.WriteLine("Enter first number:");
+            UserNumberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            UserNumberTwo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Sum first and second values: {UserNumberOne} + {UserNumberTwo} = {UserNumberOne + UserNumberTwo}");
+            Console.WriteLine($"Substruct first and second values: {UserNumberOne} - {UserNumberTwo} = {UserNumberOne - UserNumberTwo}");
+            Console.WriteLine($"Multiply first and second values: {UserNumberOne} * {UserNumberTwo} = {UserNumberOne * UserNumberTwo}");
+            Console.WriteLine($"Divide first and second values: {UserNumberOne} / {UserNumberTwo} = {UserNumberOne / UserNumberTwo}");
+            Console.WriteLine($"Show remainder of division for first and second values: {UserNumberOne} % {UserNumberTwo} = {UserNumberOne % UserNumberTwo}");
+            Console.ReadKey();
+            
+            Console.WriteLine("Another representation:");
+            Console.WriteLine($"{UserNumberOne} + {UserNumberTwo} = {UserNumberOne + UserNumberTwo}");
+            Console.WriteLine($"{UserNumberOne} - {UserNumberTwo} = {UserNumberOne - UserNumberTwo}");
+            Console.WriteLine($"{UserNumberOne} * {UserNumberTwo} = {UserNumberOne * UserNumberTwo}");
+            Console.WriteLine($"{UserNumberOne} / {UserNumberTwo} = {UserNumberOne / UserNumberTwo}");
+            Console.WriteLine($"{UserNumberOne} % {UserNumberTwo} = {UserNumberOne % UserNumberTwo}");
+            Console.ReadKey();
+
+        }
+    }
+}
