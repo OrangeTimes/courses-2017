3db0f1cee4389a1621220eb9a442ae29a30c8f3b
ezart23 <forezart@yahoo.com>
2017-04-15 00:27:36 +0300

    Homework 2

---
+++ ConsoleApplication1/CarInformation/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please provide some info about your car");
+            Console.WriteLine(); // added this to display empty line for better visual presentation
+            Console.WriteLine("Manufacturer:");
+                String Manufacturer = Console.ReadLine();
+
+            Console.WriteLine("Model:");
+                String Model = Console.ReadLine();
+
+            Console.WriteLine("Year:");
+                int Year = Convert.ToInt32(Console.ReadLine());  // however, this will throw an exception when alphabetical charecters are input here
+
+            Console.WriteLine("Price (in US dollars):");
+                double Price = Convert.ToDouble(Console.ReadLine());  // again, this will throw an exception when alphabetical charecters are input here
+
+            Console.WriteLine("Color:");
+                String Color = Console.ReadLine();
+
+            Console.WriteLine("Fuel consumption (liters per 100 km):");
+                int FuelConsumption = Convert.ToInt32(Console.ReadLine()); // will throw an exception when alphabetical charecters are input here
+
+            Console.WriteLine();
+
+            Console.WriteLine("Manufacturer: " + Manufacturer + ", " + "Model: " + Model + ", " + "Year: " + Year + ", " + "Price (in US dollars): " + Price + ", " +
+                "Color: " + Color + ", " + "Fuel consumption (liters per 100 km): " + FuelConsumption);
+            Console.ReadLine();
+
+
+        }
+    }
+}
