f94f7f21e71e2243805318cb67b82dbefe0c5033
Nazar Kulchytskyi <nazarkin124@gmail.com>
2017-04-18 12:14:46 +0300

    Update HW2,3,4 acording to Volodya's feedback

--- NazarK/NazarKSolution/HW2/ConsoleCarApplication/Program.cs
+++ NazarK/NazarKSolution/HW2/ConsoleCarApplication/Program.cs
@@ -10,24 +10,27 @@ namespace ConsoleCarApplication
     {
         static void Main(string[] args)
         {
-            string carManufacturer, carModel;
             // todo: This is a valid statement, but it is not recommnded to us it as a code convention rule. Move declarations as close to assignment as possible
-            int carYearOfManufacture, carPrice, carNumberOfDoors;
             // in scope this exercise this is correct. But in future using this prefix car in all variables names inside class is not good idea: for example in scope of a class named Car it isn't needed to have variable name: 'carManufacturer'
             // simply 'manufacturer' is enough. So it depends on context
             Console.WriteLine("Hello friend!\nPlease enter your car manufacturer:");
-            carManufacturer = Console.ReadLine();
+            string manufacturer = Console.ReadLine();
+
             // todo: I would recommend breaking up logical blocks of code by using empy lines. For example after every ReadLine
             Console.WriteLine("Please enter your car model:");
-           carModel = Console.ReadLine();
+            string model = Console.ReadLine();
+
             // todo: you need to have all code formated in vertical line
             Console.WriteLine("Please enter your car year of manufacture:");
-            carYearOfManufacture = Convert.ToInt32(Console.ReadLine());
-           Console.WriteLine("Please enter your car price:");
-            carPrice = Convert.ToInt32(Console.ReadLine());
+            int yearOfManufacture = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter your car price:");
+            double price = Convert.ToDouble(Console.ReadLine());
+
             Console.WriteLine("Please enter your car number of doors:");
-            carNumberOfDoors = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Manufacturer: {0}, Model: {1}, Year of manufacture: {2}, Price: {3}, Numder of doors: {4}", carManufacturer, carModel, carYearOfManufacture, carPrice, carNumberOfDoors);
+            int numberOfDoors = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Manufacturer: {0}, Model: {1}, Year of manufacture: {2}, Price: {3}, Numder of doors: {4}", manufacturer, model, yearOfManufacture, price, numberOfDoors);
             Console.ReadLine();
         }
     }
--- NazarK/NazarKSolution/HW3/ConsoleApplicationOperators/Program.cs
+++ NazarK/NazarKSolution/HW3/ConsoleApplicationOperators/Program.cs
@@ -15,13 +15,14 @@ namespace ConsoleApplicationOperators
             //Console.WriteLine(15 + -4 * 6 / 11);
             //Console.WriteLine(2 + 10 / 6 * 1 - 7 % 2);
 
-            int firstNumber;
-            int secondNumber;
             // todo: This is a valid statement, but it is not recommnded to us it as a code convention rule. Move declarations as close to assignment as possible
             Console.Write("Please enter first number: ");
-            firstNumber = Convert.ToInt32(Console.ReadLine());
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
             Console.Write("Please enter second number (bigger than 0): ");
-            secondNumber = Convert.ToInt32(Console.ReadLine());
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            // Show results on math operations
             Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
             Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
             Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
