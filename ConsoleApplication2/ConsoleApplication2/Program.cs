3ee3bcc9840019fd97cc225592f999c8ce070bfc
Viktoriia Strutynska <vita.ua.meister@gmail.com>
2017-04-18 15:59:58 +0300

    Updated homework 2 according to comments

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -13,12 +13,16 @@ namespace ConsoleApplication2
             Console.WriteLine("Please specify the manufacturer of the car you'd like to buy:");
             string carManufacturer = Console.ReadLine();
             // it is often a good practise to split your code into logical blocks separated by empty lines. In this case: after  each ReadLine()
+
             Console.Write("Please select the model of the car: ");
             string carModel = Console.ReadLine();
+
             Console.Write("Please specify the year of production: ");
             int carYearOfProduction = Convert.ToInt32(Console.ReadLine());
+
             Console.Write("Specify the maximum suitable price in dollars: ");
             string carMaxPrice = Console.ReadLine();
+
             Console.WriteLine("Please confirm the following information is correct. You would like to buy {0} {1}, of {2} year of production, for {3}$.", carManufacturer, carModel, carYearOfProduction, carMaxPrice);
             Console.WriteLine($"Please confirm the following information is correct. You would like to buy {carManufacturer} {carModel}, of {carYearOfProduction} year of production, for {carMaxPrice}.");
             Console.ReadKey();
