fe3c3fb632d27f3152adc14a2423a1e9572e8533
vtaben <vovataben@gmail.com>
2017-04-21 19:35:56 +0300

    3rd HW

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.
+            //int Example1Result = -1 + 3 * 5;
+            //int Example2Result = (24 + 5) % 7;
+            //double Example3Result = 15 + -4 * 6 / 11;
+            //double Example4Result = 2 + 10 / 6 * 1 - 7 % 2;
+
+            //Console.WriteLine("Example1Result: " + Example1Result);
+            //Console.WriteLine("Example2Result: " + Example2Result);
+            //Console.WriteLine("Example3Result: " + Example3Result);
+            //Console.WriteLine("Example4Result: " + Example4Result);
+            //Console.ReadKey();
+
+            //2.
+            char again = 'д';
+            while (again == 'д')
+            {
+                double a;
+                double b;
+                double total;
+                char oper;
+
+                Console.WriteLine("Enter :");
+                a = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Введите оператор:");
+                oper = Convert.ToChar(Console.ReadLine());
+
+                Console.WriteLine("Введите второе число:");
+                b = Convert.ToDouble(Console.ReadLine());
+
+                if (oper == '+')
+                {
+                    total = a + b;
+                    Console.WriteLine("Сумма {0} {1} равно {2}.", a, b, total);
+                }
+
+                else if (oper == '-')
+                {
+                    total = a - b;
+                    Console.WriteLine("Разность " + a + " и " + b + " равна " + total + ".");
+                }
+
+                else if (oper == '*')
+                {
+                    total = a * b;
+                    Console.WriteLine("Умножение " + a + " на " + b + " равно " + total + ".");
+                }
+
+                else if (oper == '/')
+                {
+                    total = a / b;
+                    Console.WriteLine("Деление " + a + " на " + b + " равно " + total + ".");
+                }
+                else
+                {
+                    Console.WriteLine("Неизвестный оператор.");
+                }
+                Console.WriteLine();
+                Console.WriteLine("Вы хотите продолжить работу с калькулятором? (д/н)");
+                again = Convert.ToChar(Console.ReadLine());
+            }
+        }
+    }
+}
---
+++ ConsoleApp3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleApp3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp3")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a95f8f13-92e7-4283-bec0-11c45c417c6f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
