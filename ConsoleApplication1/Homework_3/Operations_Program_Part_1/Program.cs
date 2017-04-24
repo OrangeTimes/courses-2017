0eb85f86bce35d94bfe45f2bbdbff3e83169cc9c
NZirchak <hereynadiia@gmail.com>
2017-04-24 20:32:11 +0300

    Homework_3

---
+++ ConsoleApplication1/Homework_3/Operations_Program_Part_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Operations_Program_Part_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			System.Console.WriteLine(-1 + 3 * 5);
+			//-1 + 15 = 14
+			System.Console.WriteLine((24 + 5) % 7);
+		    // 29 % 7 = 4(remainder of 29 / 7 = 1)
+
+		System.Console.WriteLine(15 + -4 * 6 / 11);
+			//15 - (24/11)= 15 - 2 = 13
+			System.Console.WriteLine(2 + 10 / 6 * 1 - 7 % 2);
+			//2 + (10/6) - remainder of (7/2) = 2 + 1 - 1 = 3 - 1 = 2
+			Console.ReadLine();
+		}
+	}
+}
---
+++ ConsoleApplication1/Homework_3/Operations_Program_Part_2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Operations_Program_Part_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int x, y;
+			char operation;
+
+			// Example: Input first number: 12
+						// Input operation: +
+						// Input second number: 3
+						// Expected Output : 12 + 3 = 15
+
+			Console.Write("Input first number: ");
+			x = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Input operation: ");
+			operation = Convert.ToChar(Console.ReadLine());
+			Console.Write("Input second number: ");
+			y = Convert.ToInt32(Console.ReadLine());
+
+			if (operation == '+')
+				Console.WriteLine("{0} + {1} = {2}", x, y, x + y);
+			else if (operation == '-')
+				Console.WriteLine("{0} - {1} = {2}", x, y, x - y);
+			else if ((operation == 'x') || (operation == '*'))
+				Console.WriteLine("{0} * {1} = {2}", x, y, x * y);
+			else if (operation == '/')
+				Console.WriteLine("{0} / {1} = {2}", x, y, x / y);
+			else if (operation == '%')
+				Console.WriteLine("{0} mod {1} = {2}", x, y, x % y);
+			else
+				Console.WriteLine("Incorrect Character");
+			Console.ReadLine();
+
+			// Another implementation
+			// all operations will be displayed after entering two numbers: Example: Input first number: 6
+																				// Input second number: 6
+																				// 6 + 6 = 12
+																				// 6 - 6 = 0
+																				// 6 * 6 = 36
+																				// 6 / 6 = 1
+																				// 6 mod 6 = 0
+			Console.Write("Input first number: ");
+			int a = Convert.ToInt32(Console.ReadLine());
+
+			Console.Write("Input second number: ");
+			int b = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+			Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
+			Console.WriteLine("{0} x {1} = {2}", a, b, a * b);
+			Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
+			Console.WriteLine("{0} mod {1} = {2}", a, b, a % b);  
+			Console.ReadLine();
+		}
+	}
+}
+
+	
