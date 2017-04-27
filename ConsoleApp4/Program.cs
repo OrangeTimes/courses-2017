75a27ab54dd08750ec0928872d7fd37130d47f6e
vtaben <vovataben@gmail.com>
2017-04-27 12:36:41 +0300

    Added 1st part of HW4

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            Console.WriteLine("Enter your first number:");
+            int int1 = Convert.ToInt32 (Console.ReadLine());       
+            Console.WriteLine("Enter your second number:");
+            int int2 = Convert.ToInt32 (Console.ReadLine());
+
+            bool Output = int1 % int2 == 0 ? true : false;
+
+            Console.WriteLine("Output:" + Output);
+            Console.ReadKey();
+        }
+    }
+}
