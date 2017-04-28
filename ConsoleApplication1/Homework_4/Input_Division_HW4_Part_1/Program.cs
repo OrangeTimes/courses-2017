7f2704a1f5f899934a5431473765706082cdc762
NZirchak <hereynadiia@gmail.com>
2017-04-28 12:20:29 +0300

    Homework_4

---
+++ ConsoleApplication1/Homework_4/Input_Division_HW4_Part_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Homework_4_Part_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Write a program to check if user input is divisible by some number also enter by the user, write the result to console.
+			// Example of input: 10 , 2
+			// Output: true
+			Console.WriteLine("Input first number:");
+			int input1 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Input second number:");
+			int input2 = Convert.ToInt32(Console.ReadLine());
+			bool isDivisible = false;
+			if (input1 % input2 == 0)
+			{
+				isDivisible = true;
+			}
+			Console.WriteLine($"Number is divisible: {isDivisible}");
+			Console.ReadLine();
+		}
+	}
+}
