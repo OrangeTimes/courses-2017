aae25b24363516d27eec78c8033e5213a12af17c
Oleg Galush <oleggalushh@gmail.com>
2017-05-09 00:35:34 +0300

    HW4 exercise 1

---
+++ Halush/HomeWorks/HW4/HW4_1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4
+{
+    class HW4_1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number:");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+            Boolean isDivisible = numberOne % numberTwo == 0;
+            Console.WriteLine("Is {0} devisilbe by {1}? {2}", numberOne, numberTwo, isDivisible);
+            Console.ReadKey();
+        }
+    }
+}
