e7709cb9ae00e51721f26e66fa21f8da4261951d
Oleg Galush <oleggalushh@gmail.com>
2017-05-08 22:19:02 +0300

    HW2 Done/Added

---
+++ Halush/HomeWorks/HW2/HW2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2
+{
+    class HW2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Manufacturer:");
+            String carManufacturer = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter Model:");
+            String carModel = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter Year:");
+            int carYear = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Price:");
+            Double carPrice = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Color:");
+            String carColor = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("You selected Manufacturer:{0}, Model:{1}, Year:{2}, Price:{3}$, Color:{4}.", carManufacturer, carModel, carYear, carPrice, carColor);
+            Console.ReadKey();
+        }
+    }
+}
