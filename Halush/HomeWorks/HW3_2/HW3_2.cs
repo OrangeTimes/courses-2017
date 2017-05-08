ff7b85d776622e9dc2f5f8e270d1d9c0d382a7ce
Oleg Galush <oleggalushh@gmail.com>
2017-05-08 23:03:23 +0300

    HW3 Done/Added

---
+++ Halush/HomeWorks/HW3_2/HW3_2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3_2
+{
+    class HW3_2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number:");
+            double numberOne = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            double numberTwo = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("{0}+{1}={2}", numberOne, numberTwo, numberOne + numberTwo);
+            Console.WriteLine("{0}-{1}={2}", numberOne, numberTwo, numberOne - numberTwo);
+            Console.WriteLine("{0}*{1}={2}", numberOne, numberTwo, numberOne * numberTwo);
+            Console.WriteLine("{0}/{1}={2}", numberOne, numberTwo, numberOne / numberTwo);
+            Console.WriteLine("{0}%{1}={2}", numberOne, numberTwo, numberOne % numberTwo);
+            Console.ReadKey();
+        }
+    }
+}
