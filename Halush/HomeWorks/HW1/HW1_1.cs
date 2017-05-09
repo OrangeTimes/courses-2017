8f0a0d1e6e91f8ba0de23548758f52a58f4148db
Oleg Galush <oleggalushh@gmail.com>
2017-05-09 17:06:00 +0300

    HW1 added

---
+++ Halush/HomeWorks/HW1/HW1_1.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace HW1
+{
+    class HW1_1
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class TestClass
+    {
+        public static JObject Test()
+        {
+            JObject obj = new JObject();
+            return obj;
+        }
+    }
+}
+
---
+++ Halush/HomeWorks/HW1_2/HW1_2.cs
@@ -0,0 +1,21 @@
+﻿using HW1;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1_2
+{
+    class HW1_2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calling another assembly");
+            TestClass.Test();
+            Console.WriteLine("Success");
+            Console.ReadKey();
+        }
+    }
+}
