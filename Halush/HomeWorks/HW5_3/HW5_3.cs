52c787b2c1197694fe855ef31e0433df1294291d
Oleg Galush <oleggalushh@gmail.com>
2017-05-10 23:48:45 +0300

    HW5_3 Done/Added

---
+++ Halush/HomeWorks/HW5_3/HW5_3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+Write a program that will read user name and will check if it is a valid one (has more than 0 symbols and less than 50 symbols). 
+If the input is invalid it should write an error message and prompt for user name again until user enters a valid name.
+*/
+namespace HW5_3
+{
+    class HW5_3
+    {
+        static void Main(string[] args)
+        {
+            enterName:
+            Console.WriteLine("Please enter your name:");
+            string userName = Console.ReadLine();
+            int nameLenght = userName.Length;           //Defining name's lenght
+            if (nameLenght > 50)
+            {
+                Console.WriteLine("User name must have more then 0 and less then 51 characters.");
+                goto enterName;
+            }
+            if (nameLenght < 1)
+            {
+                Console.WriteLine("User name must have more then 0 and less then 51 characters.");
+                goto enterName;
+            }
+            else {
+                Console.WriteLine("Welcome {0}", userName);
+                Console.ReadKey();
+            }
+        }
+    }
+}
