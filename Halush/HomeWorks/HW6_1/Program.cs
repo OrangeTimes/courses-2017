8bbbb970a83a8c45a35d7ab0bfb4412ca5e1e01f
Oleg Galush <oleggalushh@gmail.com>
2017-05-15 00:11:47 +0300

    HW 6_1 & 6_2 Done/Added

---
+++ Halush/HomeWorks/HW6_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+Write a program to get a double value from user. 
+Determine if that number is whole or real number by outputting corresponding message (use casting).
+*/
+
+namespace HW6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your number:");
+            string userValue = Console.ReadLine();
+            bool isValueCorrect = Double.TryParse(userValue, out double convertedValue); //Verify that user's value can be successfully converted
+            while (isValueCorrect == false)                                              //Until user's value is incorrect ask to enter the value again
+            {
+                Console.WriteLine("You entered an incorrect value.");
+                Console.WriteLine("Please enter your number:");
+                userValue = Console.ReadLine();
+                isValueCorrect = Double.TryParse(userValue, out convertedValue);
+            }
+            {                                                                            //When user entered a correct value determine if it is a whole or a real by containing a ","
+                if (userValue.IndexOf(",") >= 0)
+                {
+                    Console.WriteLine("Your number {0} is real", convertedValue);
+                }
+                else
+                {
+                    Console.WriteLine("Your number {0} is whole", convertedValue);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Halush/HomeWorks/HW6_2/HW6_2.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Write a program what supports multiple casting abilities for entered value by user.
+After entering a value, you should ask about target type. 
+Supported target types: are “byte, short, integer, long”. 
+If entered target type is supported your program should display result of the cast or unsupported target type message otherwise.
+
+Example of user input and output:
+100
+integer
+100
+
+100.3
+double
+Unsupported target type
+*/
+
+namespace HW6_2
+{
+    class HW6_2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your number:");
+            string userValue = Console.ReadLine();
+            bool isValueProper = Double.TryParse(userValue, out double convertToDouble);     //Verify that user's value can be successfully converted
+            while (isValueProper == false)                                                   //Until user's value is incorrect ask him to enter a value again
+            {
+                Console.WriteLine("Please enter correct value:");
+                userValue = Console.ReadLine();
+                isValueProper = Double.TryParse(userValue, out convertToDouble);
+            }                                                                                //If user enetered a correct value ask him for a type he wants to get
+            Console.WriteLine("What is your target type? Please enter type within byte, short, integer, long:");
+            string targetType = Console.ReadLine();
+            switch (targetType)                                                              //List of types user can choose 
+            {
+                case "byte":
+                    {
+                        sbyte byteValue = (sbyte)convertToDouble;
+                        Console.WriteLine("Your value after converting to byte is {0}", byteValue);
+                        Console.ReadKey();
+                        break;
+                    }
+                case "short":
+                    {
+                        short shortValue = (short)convertToDouble;
+                        Console.WriteLine("Your value after converting to short is {0}", shortValue);
+                        Console.ReadKey();
+                        break;
+                    }
+                case "integer":
+                    {
+                        int intValue = (int)convertToDouble;
+                        Console.WriteLine("Your value after converting to integer is {0}", intValue);
+                        Console.ReadKey();
+                        break;
+                    }
+                case "long":
+                    {
+                        long longValue = (long)convertToDouble;
+                        Console.WriteLine("Your value after converting to long is {0}", longValue);
+                        Console.ReadKey();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("The {0} type is not supported", targetType);
+                        Console.ReadKey();
+                        break;
+                    }
+            }
+        }
+    }
+}
