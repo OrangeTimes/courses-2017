e681e2962683b494e5fda0cb43684b235f0ce6d3
Oleg Galush <oleggalushh@gmail.com>
2017-05-21 13:25:16 +0300

    7_1

---
+++ Halush/HomeWorks/HW7_1/HW7_1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+You are developing postal delivery application write a program to get info of delivery method and display it using enums. (User input can be provided as number)
+*/
+
+enum PostDelivery
+{
+    ByAir,
+    BySea,
+    ByCar,
+    PostToPost,
+    PostToHome
+}
+
+namespace HW7_1
+{
+    class HW7_1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please select postal delivery method:");
+            string userInput = Console.ReadLine();
+        }
+    }
+}
