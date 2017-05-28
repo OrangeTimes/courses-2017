d10071530227e877c950e5ee7af33646693eac4c
Oleg Galush <oleggalushh@gmail.com>
2017-05-28 10:12:16 +0300

    HW 8 Done / Added

---
+++ Halush/HomeWorks/HW8_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Write a C# program to get average value of elements in jagged integer array 
+*/
+
+namespace HW8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jaggedArray = {                  //Jagged array declarationa dn initialization
+               new int[] {1,2,3},
+               new int[] {4,5},
+               new int[] {6,7,8,9,10},
+               new int[] {11,12}
+            };
+            double sumOfElements = 0;
+            double amountOfElements = 0;
+
+            for (int i = 0; i < jaggedArray.Length; i++)             //Iterating through each each array of arrays
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)      //Iterating through each element of each array of arrays
+                {
+                    sumOfElements += jaggedArray[i][j];              //Counting sum of elements
+                }
+                amountOfElements += jaggedArray[i].Length;           //Counting amount of elements in each array of arrays
+            }
+            Console.WriteLine("Sum of Elements: {0}", sumOfElements);
+            Console.WriteLine("Amount of elements: {0}", amountOfElements);
+            Console.WriteLine("Average value of elements: {0}", sumOfElements / amountOfElements);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Halush/HomeWorks/HW8_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Print out all values in reversed order(starting from last element) in jagged or multidimensional array 
+ */
+
+namespace HW8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] multiArray = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+            int dimension1 = multiArray.GetLength(0) - 1;      //Get the first dimension lenght 
+            int dimension2 = multiArray.GetLength(1) - 1;      //Get the second dimension lenght
+            for (int i = dimension1; i >= 0; i--)              //Iterate through the first dimension in reverse order
+            {
+                for (int j = dimension2; j >= 0; j--)          //Iterate through the second dimension in reverse order
+                {
+                    Console.WriteLine(multiArray[i, j]);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
