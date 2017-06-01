a18d24ce285428cb401bd5fbe974e1d3688182bc
Yevgen Kramarets <ekramarets@gmail.com>
2017-06-01 23:25:47 +0300

    added HW4. still need to fix 4.2 as flow is not clear

---
+++ ConsoleApplication1/HW4/IsDivisibleCheck/IsDivisibleCheck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IsDivisibleCheck
+{
+    class IsDivisibleCheck
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Value 1");
+            int valueOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Value 2");
+            int valueTwo = Convert.ToInt32(Console.ReadLine());
+
+            bool isDivisibleCheck = valueOne % valueTwo == 0;
+
+            Console.WriteLine(isDivisibleCheck);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication1/HW4/RoomLights/RoomLights.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomLights
+{
+    class RoomLights
+    {
+        static void Main(string[] args)
+        {
+            // int roomLights = 0; // 0000 - lights off
+
+            // to enable light # 1, 3, 5 enter 0001, 0011, 0101
+
+            int roomLights = 0; //00000
+            Console.WriteLine("Enter # of light you want to enable: ");
+            int LightNumber = Convert.ToInt32(Console.ReadLine(), 2);
+
+            roomLights |= LightNumber;
+
+            string lightState = Convert.ToString(roomLights, 2);
+
+            Console.WriteLine($"Light # {lightState} is on ");
+            Console.ReadKey();
+
+        }
+    }
+}
