20fbbd3512250eee5829118349827152d9187a4b
Volodymyr Golub <volodymyr.golub@verint.com>
2017-06-02 13:17:37 +0300

    Added practical # 8

---
+++ Practical07/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical07
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//MultiDimensional();
+			int[][] jaggedArray = new[]
+			{
+				new[] {1},
+				new[] {3, 4, 5},
+				new[] {3, 4, 9, 10, 20},
+				new[] {3, 4, 9, 10, 20, 789, 4567705},
+			};
+
+			foreach (int[] i in jaggedArray)
+			{
+				foreach (int j in i)
+				{
+					Console.WriteLine(j);
+				}
+			}
+
+			for (int i = 0; i < jaggedArray.Length; i++)
+			{
+				for (int j = 0; j < jaggedArray[i].Length; j++)
+				{
+					Console.WriteLine(jaggedArray[i][j]);
+				}
+			}
+
+			Console.ReadKey();
+
+		}
+
+		private static void MultiDimensional()
+		{
+			int[,] multiDimensional = new int[,]
+			{
+				{54, 5, 4},
+				{10, 7, 9},
+				{3, 5, 10}
+			};
+
+			//foreach (int i in multiDimensional)
+			//{
+			//	Console.WriteLine(i);
+			//}
+			int dimensionCount = multiDimensional.Rank;
+			for (int i = 0; i < multiDimensional.GetLength(0); i++)
+			{
+				for (int j = 0; j < multiDimensional.GetLength(1); j++)
+				{
+					Console.WriteLine(multiDimensional[i, j]);
+				}
+			}
+
+			Console.WriteLine("Length:");
+			Console.WriteLine(multiDimensional.Length);
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Practical08/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical08
+{
+	public enum ExponentType
+	{
+		UseExponent,
+		NotUseExponent
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double result = Substract(1, 2);
+			MethodA();
+		}
+
+		public static double Substract(double number1, double number2, ExponentType exponentType)
+		{
+			return number1 - number2;
+		}
+
+		public static double Substract(double number1, double number2)
+		{
+			return number1 - number2;
+		}
+
+		public static int Substract(int number1, int number2)
+		{
+			return number1 - number2;
+		}
+
+		public static long Substract(long number1, long number2)
+		{
+			return number1 - number2;
+		}
+
+		public double Substract(double number1)
+		{
+			return number1;
+		}
+
+		public static void MethodA()
+		{
+			MethodB();
+		}
+
+		public static void MethodB()
+		{
+			MethodC();
+		}
+
+		public static void MethodC()
+		{
+			int x = 0;
+		}
+	}
+}
