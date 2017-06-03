c4c4610b1e11fd0a16f6bd66b66ec56712b08b99
Yevgen Kramarets <ekramarets@gmail.com>
2017-06-03 23:51:12 +0300

    added HW 6.1 & 6.2

---
+++ ConsoleApplication1/HW6/DoubleCasting/DoubleCasting.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleCasting
+{
+    class DoubleCasting
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter custom number: ");
+            double userValue = Convert.ToDouble(Console.ReadLine());
+
+            if (userValue == 0)
+            {
+                Console.WriteLine($"You have entered {userValue} which is WHOLE and REAL at same time");
+            }
+
+            else if (userValue == (int)userValue && userValue > 0)
+            {
+                Console.WriteLine($"You have entered {userValue} which is WHOLE");
+            }
+
+            else
+            {
+                Console.WriteLine($"You have entered {userValue} which is REAL");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication1/HW6/MultipleCasting/MultipleCasting.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleCasting
+{
+    class MultipleCasting
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Value to convert: ");
+            string valueToConvert = Console.ReadLine();
+
+            Console.WriteLine(@"Enter Type to convert (select between ""byte"", ""short"", ""integer"", ""long""): ");
+            string typeToConvert = Console.ReadLine();
+
+            switch (typeToConvert)
+            {
+                case "byte":
+                    Console.WriteLine($"{Convert.ToByte(valueToConvert)}");
+                    break;
+
+                case "short":
+                    Console.WriteLine($"{Convert.ToInt16(valueToConvert)}");
+                    break;
+
+                case "integer":
+                    Console.WriteLine($"{Convert.ToInt32(valueToConvert)}");
+                    break;
+
+                case "long":
+                    Console.WriteLine($"{Convert.ToInt64(valueToConvert)}");
+                    break;
+
+                default:
+                    Console.WriteLine("Unsupported target type.");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
