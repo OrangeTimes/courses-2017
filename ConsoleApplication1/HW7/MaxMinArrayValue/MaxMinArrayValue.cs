0aa565bb4a63fd13786a774805857f7bd98327a2
Yevgen Kramarets <ekramarets@gmail.com>
2017-06-04 00:00:43 +0300

    added placeholders for HW7

---
+++ ConsoleApplication1/HW7/MaxMinArrayValue/MaxMinArrayValue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxMinArrayValue
+{
+    class MaxMinArrayValue
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ ConsoleApplication1/HW7/PostalDeliveryApp/PostalDeliveryApp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostalDeliveryApp
+{
+    class PostalDeliveryApp
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
