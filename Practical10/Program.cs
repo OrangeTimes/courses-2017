c218f18c5706109939d1cbdd3fc52a7d6fb80ea0
Volodymyr Golub <volodymyr.golub@verint.com>
2017-07-03 10:02:38 +0300

    Added Practical #10

---
+++ Practical10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical10
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Displaying passed in args:");
+			GetValue(args);
+			Console.ReadKey();
+		}
+
+		private static void GetValue(string[] args)
+		{
+			int x = 0;
+			foreach (string arg in args)
+			{
+				Console.WriteLine("Value of arg:" + arg);
+			}
+		}
+	}
+}
