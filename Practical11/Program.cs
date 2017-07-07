3a1b95bad113e45944e5920da77da0872705d854
Volodymyr Golub <volodymyr.golub@verint.com>
2017-07-07 10:19:24 +0300

    Added practical 11

---
+++ Practical11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical11
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int i = 0; i < 99; i++)
+			{
+				int x = 0;
+			}
+
+			MethodA(new []{8});
+
+		}
+
+		public static void MethodA(int[] value)
+		{
+			int x = 0;
+			MethodB();
+		}
+
+		public static void MethodB()
+		{
+			int x = 0;
+			MethodC();
+		}
+
+		public static void MethodC()
+		{
+			int x = 0;
+		}
+	}
+}
