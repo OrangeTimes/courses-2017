85dcb1e7e23a7b5780adfaf9104eb41c67e06cbf
Nazar Kulchytskyi <nazarkin124@gmail.com>
2017-08-03 00:26:15 +0300

    Adding HW12

---
+++ NazarK/NazarKSolution/HW12/Exception Handling_1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Exception_Handling_1
+{
+	// Write a program to check if a string is a valid number(double) using Double.
+	// Parse and show corresponding error to user.
+	// Try to recover from this exception by asking user for valid input one more time
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Ask user for input until it's valid
+			bool isValidDouble;
+			do
+			{
+				// Ask for a string 
+				Console.Write("Enter a string to check if it's a Double: ");
+				string userInput = Console.ReadLine();
+
+				// Check if it as a valid Double
+				isValidDouble = MyCustomTryParse(userInput);
+
+				// Form and print the message based on if string was Double or not
+				string validationMessage = isValidDouble ? "Input is a valid Double" : "Input is NOT a valid Double, try again.\n";
+				Console.WriteLine(validationMessage);
+			}
+			while (!isValidDouble);
+		}
+
+		static bool MyCustomTryParse(string input)
+		{
+			try
+			{
+				// Try to parse string to Double
+				Double.Parse(input);
+			}
+			// catch all kinds of exceptions and return false because it's not convertible
+			catch (ArgumentNullException e)
+			{
+				Console.WriteLine(MyFancyErrorMessage(e));
+				return false;
+			}
+			catch (FormatException e)
+			{
+				Console.WriteLine(MyFancyErrorMessage(e));
+				return false;
+			}
+			catch (OverflowException e)
+			{
+				Console.WriteLine(MyFancyErrorMessage(e));
+				return false;
+			}
+			// return true if there was no exceptions which means that it is convertible
+			return true;
+		}
+
+		static string MyFancyErrorMessage(Exception e)
+		{
+			return "\n" + e.Message + "\n" + e.StackTrace + "\n";
+		}
+	}
+}
---
+++ NazarK/NazarKSolution/HW12/Exception Handling_2/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Security;
+using System.Security.Permissions;
+
+namespace Exception_Handling_2
+{
+	// Write an application to show content of .txt file(use File.ReadAllText()) to console.
+	// Display proper messages to user in edge scenarios (file not present, empty path)
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// correct file path and name
+			WriteAllTextToConsole("Hello World.txt");
+
+			// 1 path is null
+			WriteAllTextToConsole(null);
+
+			// 2 empty path
+			WriteAllTextToConsole("");
+
+			// 3 file name is longer then 260 symbols
+			WriteAllTextToConsole("Is post each that just leaf no. He connection interested so we an sympathize advantages. To said is it shed want do. Occasional middletons everything so to. Have spot part for his quit may. Enable it is square my an regard. Often merit stuff first oh up hills.");
+
+			// 4 folder doesn't exist
+			WriteAllTextToConsole(@"fake/fake.txt");
+
+			// 5 file doesn't exist
+			WriteAllTextToConsole("fake.txt");
+
+			// 6 Access Denied because file is actually a folder
+			WriteAllTextToConsole("FakeFolder");
+
+			// 7 Access Denied because file is being used by another program
+			// adding 2 more statements to open and close stream, and True to not to overwrite a file
+			StreamWriter writer = new StreamWriter("Hello World.txt", true);
+			WriteAllTextToConsole("Hello World.txt");
+			writer.Close();
+
+			// 8 Added invisible character to file name so that it will fail
+			WriteAllTextToConsole(@"‪C:\Test.txt");
+
+			// 9 Causes any calling code for any PermissionSet that is not a subset of the current PermissionSet to fail
+			PermissionSet perms = new PermissionSet(null);
+			perms.PermitOnly();
+			WriteAllTextToConsole("Hello World.txt");
+		}
+
+		static void WriteAllTextToConsole(string path)
+		{
+			try
+			{
+				Console.WriteLine(File.ReadAllText(path));
+			}
+
+			catch (ArgumentNullException e)
+			{
+				Console.WriteLine("\nCatch 1: " + e.Message);
+			}
+			catch (ArgumentException e)
+			{
+				Console.WriteLine("\nCatch 2: " + e.Message);
+			}
+			catch (PathTooLongException e)
+			{
+				Console.WriteLine("\nCatch 3: " + e.Message);
+			}
+			catch (DirectoryNotFoundException e)
+			{
+				Console.WriteLine("\nCatch 4: " + "Check if folder exists. " + e.Message);
+			}
+			catch (FileNotFoundException e)
+			{
+				Console.WriteLine("\nCatch 5: " + e.Message);
+			}
+			catch (UnauthorizedAccessException e)
+			{
+				Console.WriteLine("\nCatch 6: " + e.Message);
+			}
+			catch (IOException e)
+			{
+				Console.WriteLine("\nCatch 7: " + e.Message);
+			}
+			catch (NotSupportedException e)
+			{
+				Console.WriteLine("\nCatch 8: " + e.Message);
+			}
+			catch (SecurityException e)
+			{
+				Console.WriteLine("\nCatch 9: " + "You have no permission to read from this file. " + e.Message);
+			}
+		}
+	}
+}
