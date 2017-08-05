3a7b15bcd77d6cf4d8becfc65dde9025fca36b8f
vika4ka <vita.ua.meister@gmail.com>
2017-08-05 14:11:32 +0300

    adding homework 8.2

---
+++ ConsoleApplication2/Homework8.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. Print out all values in reversed order(starting from last element) in jagged or multidimensional array
+
+            int [][] myJaggedArray = { new int[] { 4, 3, 5, 2 }, new int[] { 2, 5, 4, 9, 8, 5, 8 }, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 } };
+
+            for (int i = myJaggedArray.Length - 1; i >= 0; i--)
+            {
+                for (int j = myJaggedArray[i].Length - 1; j >= 0; j--)
+                {
+                    int number = myJaggedArray[i][j];
+                    Console.Write(number);
+                }
+            }
+            Console.ReadLine();          
+
+        }
+    }
+}
