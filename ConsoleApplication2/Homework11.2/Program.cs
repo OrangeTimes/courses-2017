7d4f1609b0af20ed047a5350f6a84bbee2ea64d8
vika4ka <vita.ua.meister@gmail.com>
2017-08-07 13:00:50 +0300

    Adding Homework 11.1

---
+++ ConsoleApplication2/Homework11.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework11._2
+{
+    class Program
+    {
+        // 2.	 Write a program to write the best profit you could have made from 1 purchase and 1 sale buying and selling specific book in 1 week. 
+        // a.Indexes in array indicate days past start day: [2] – Indicate that 2 days past from the start of week
+        //b.The values are the book price in dollars at that day
+        //c.You must buy before you sell. You may not buy and sell in the same day.
+        //d.Try to iterate over array in most effective way
+        //Example of input: int[] bookPricesLastWeek = { 11, 9, 6, 8, 13, 7, 10 }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ ConsoleApplication2/Homework11/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.	 Write a program to create isosceles triangle in console by specifying its height 
+            //a.Value of height should be passed as command line parameter
+            //Example for height of 4:
+ /*             *
+               ***
+              *****
+             *******    */
+
+             // ask for triangle height
+           Console.Write("Please specify height of triangle: ");
+           int height = Convert.ToInt32(Console.ReadLine());
+ 
+            //print rows
+            for (int rowNumber = 1; rowNumber <= height; rowNumber++)
+            {
+                Console.WriteLine(BuildTriangle(height, rowNumber));
+            }
+            Console.ReadLine();
+        }
+
+        static string BuildTriangle(int height, int rowNumber)
+        {
+            int numberOfDots = rowNumber * 2 - 1;
+            int numberOfSpaces = height - rowNumber;
+            
+            // create instance of stringbuilder to append characters in string
+            StringBuilder myStringBuilder = new StringBuilder();
+
+            myStringBuilder.Append(' ', numberOfSpaces);
+            myStringBuilder.Append('*', numberOfDots);
+
+            return myStringBuilder.ToString();
+        }
+         
+    }
+}
