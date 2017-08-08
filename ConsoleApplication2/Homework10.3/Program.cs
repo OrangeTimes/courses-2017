9eabd02f53c0b304c7526c842f62edce44af9634
vika4ka <vita.ua.meister@gmail.com>
2017-08-08 11:43:06 +0300

    Updated Homework 10.3 with optional params

--- ConsoleApplication2/Homework10.3/Program.cs
+++ ConsoleApplication2/Homework10.3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,7 @@ namespace Homework10._3
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             // 3.Create a program which can write user input to a file. 
             //If user didn’t specify extension.txt extension should be used.
@@ -18,23 +19,36 @@ namespace Homework10._3
             // ask user to add text and file extension
             Console.WriteLine("Please add text: ");
             string userInput = Console.ReadLine();
+            Console.WriteLine("Specify path: ");
+            string path = Console.ReadLine();
+            Console.WriteLine("Please file name: ");
+            string fileName = Console.ReadLine();
             Console.WriteLine("Specify file extension: ");
             string fileExtension = Console.ReadLine();
 
+            string filePathAndName = path + fileName;
+            
+            
             //check if file extension is provided
-            bool extensionNotSpecified = String.IsNullOrEmpty(fileExtension);
-
-            //if file extension not provided - add txt extention
-            if (extensionNotSpecified == true)
-            {
-                fileExtension = "txt";
-            }
-
-            //form filePath with extension
-            string filePath = "C:/1." + fileExtension;
+            bool extensionSpecified = String.IsNullOrEmpty(fileExtension);
 
             //create file and write text to it
-            File.WriteAllText(filePath, userInput);
+            if (extensionSpecified == true)
+            {
+                CreateFileWithText(userInput, filePathAndName);
+            }
+            else
+            {
+                CreateFileWithText(userInput, filePathAndName, fileExtension);
+            }
         }
+
+        static void CreateFileWithText(string userInput, string pathAndName, string fileExtension = ".txt")
+        {
+            string filePathNameExtension = pathAndName + fileExtension;
+            File.WriteAllText(filePathNameExtension, userInput);
+        }
+
+
     }
 }
--- ConsoleApplication2/Homework11.2/Program.cs
+++ ConsoleApplication2/Homework11.2/Program.cs
@@ -8,8 +8,9 @@ namespace Homework11._2
 {
     class Program
     {
-        // 2.	 Write a program to write the best profit you could have made from 1 purchase and 1 sale buying and selling specific book in 1 week. 
-        // a.Indexes in array indicate days past start day: [2] – Indicate that 2 days past from the start of week
+        // 2.	 Write a program to write the best profit you could have made from 1 purchase and 1 sale 
+        //buying and selling specific book in 1 week. 
+        //a.Indexes in array indicate days past start day: [2] – Indicate that 2 days past from the start of week
         //b.The values are the book price in dollars at that day
         //c.You must buy before you sell. You may not buy and sell in the same day.
         //d.Try to iterate over array in most effective way
