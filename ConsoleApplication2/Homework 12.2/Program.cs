1811d503bf4dd2718c9dcb1be92f20dd8944ab61
vika4ka <vita.ua.meister@gmail.com>
2017-08-11 21:16:09 +0300

    Adding Homework 12.1

---
+++ ConsoleApplication2/Homework 12.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_12._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. Write an application to show content of txt file (use File.ReadAllText()) to console. 
+            //Display proper messages to user in edge scenarios (file not present, empty path) 
+        }
+    }
+}
---
+++ ConsoleApplication2/Homework12.1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework12._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.Write a program to check if a string is a valid number (double) using Double.
+            // Parse and show corresponding error to user.
+            // Try to recover from this exception by asking user for valid input one more time
+
+            Console.Write("Please enter value: ");
+            string userInput = Console.ReadLine();
+
+            bool stringIsValid;
+
+            do
+            {
+                stringIsValid = CheckIfValid(userInput);
+
+                if (stringIsValid == true)
+                {
+                    Console.WriteLine("Thanks");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.Write("Please enter value again: ");
+                    userInput = Console.ReadLine();
+                    stringIsValid = CheckIfValid(userInput);
+                }
+
+            } while (stringIsValid == false);
+
+
+        }
+
+        static bool CheckIfValid(string userInput)
+        {
+            try
+            {
+                if (userInput == "")
+                {
+                    throw new System.ArgumentException("Response is required.");
+                }
+                Convert.ToDouble(userInput);
+            }
+            catch (System.ArgumentException responseRequiredException)
+            {
+                Console.WriteLine(responseRequiredException.ToString());
+                Console.ReadLine();
+                return false;
+            }
+            catch (System.FormatException invalidFormatException)
+            {
+                Console.WriteLine(invalidFormatException.ToString());
+                Console.ReadLine();
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadLine();
+                return false;
+            }
+            return true;
+        }
+    }
+}
