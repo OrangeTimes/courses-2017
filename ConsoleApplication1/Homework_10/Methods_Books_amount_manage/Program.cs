03bfca29e079cbbb073cd5064d89e2929090ad1b
NZirchak <hereynadiia@gmail.com>
2017-08-14 13:56:44 +0300

    Homework_10 (part1 + part2)

---
+++ ConsoleApplication1/Homework_10/Methods_Books_amount_manage/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Books_amount_manage
+{
+	class Program
+	// Write a program which manages books amount.You will need init it with starting amount of books.
+	// And write 2 methods which can increase and decrease this amount. (use ref or out to implement this functionality)
+	{
+		static void Main(string[] args)
+		{
+			int booksAmount = 32;
+			Console.WriteLine($"Initial amount of books: {booksAmount}");
+			Console.WriteLine();
+			// increase amount of books
+		Increase(ref booksAmount,6);
+			Console.WriteLine($"Increased amount of books: {booksAmount}");
+			// decrease amount of books
+			Decrease(ref booksAmount,8);
+			Console.WriteLine($"Decreased amount of books: {booksAmount}");
+			Console.ReadLine();
+		}
+		static void Increase(ref int booksAmount, int increaseAmount)
+		{
+			booksAmount += increaseAmount;
+		}
+		static void Decrease(ref int booksAmount, int decreaseAmount)
+		{
+			booksAmount -= decreaseAmount;
+		}
+	}
+}
---
+++ ConsoleApplication1/Homework_10/Methods_Sum_of_multiple_params/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Sum_of_multiple_params
+{
+	class Program
+	{
+		// Write a C# program which supports calculation of sum for 2 doubles,
+		// 2 integers, any number of integers and doubles passed in as
+		// a parameters (Hint: use parameters to support multiple arguments)
+		static void Main(string[] args)
+		{
+			int resultOfIntSum = sumValues(5, 7);
+			double resultOfDoubleSum = sumValues(5.5465656, 7.56537567); // 
+
+			int[] intArray = new int[] { 134, 2, 32, 44 };
+			int resultOfIntArraySum = sumValues(intArray);
+
+			double[] doubleArray = new double[] { 1.4344345, 4.45645656, 3.3534, 8.5734 };
+			double resultOfDoubleArraySum = sumValues(doubleArray);
+		}
+		static int sumValues(int param1, int param2)
+		{
+			int sum = param1 + param2; //calculate sum of two int
+			return sum; 
+		}
+		static double sumValues(double param1, double param2)
+		{
+			double sum = param1 + param2; // calculate sum of two double
+			return sum; 
+		}
+		static int sumValues(params int[] intArray)
+		{
+			int sum = 0;
+			foreach (var number in intArray)
+			{
+				sum += number; // calculate sum of int array
+			}
+			return sum; 
+		}
+		static double sumValues(params double[] doubleArray)
+		{
+			double sum = 0;
+			foreach (var number in doubleArray)
+			{
+				sum += number; // calculate sum of double array
+			}
+			return sum; 
+		}
+	}
+}
