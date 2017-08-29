42034f5a445d7fa3bee9ba655e8a801a0f9d8807
Roman Yaremko <Roman.Yaremko@verint.com>
2017-08-29 14:42:05 +0300

    Adding Homework 12

---
+++ ConsoleApplication1/Homework 12/HM12 Ex.1 Is a string a valid number/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM12_Ex._1_Is_a_string_a_valid_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckString();
+            Console.ReadLine();
+
+        }
+
+        /*
+        1. Write a program to check if a string is a valid number (double) using Double.Parse and show corresponding error to user. 
+        Try to recover from this exception by asking user for valid input one more time
+        */
+
+        /// <summary>
+        /// Checking if input string is a valid number (double) using Double.Parse and show corresponding error to user
+        /// </summary>
+        /// <returns>Double value</returns>
+        public static double CheckString()
+        {
+            double myDouble;
+            Console.WriteLine("Please enter string to convert it to double: ");
+            try
+            {
+                myDouble = Double.Parse(Console.ReadLine(), System.Globalization.NumberStyles.AllowDecimalPoint);  // Converting to double, added System.Globalization.NumberStyles.AllowDecimalPoint as without it method converts incorrectly (e.g. 12,12 to 1212)
+                Console.WriteLine("Successfully converted to: " + $"{myDouble}");                                  // Printing out double value 
+                return myDouble; 
+            }
+            catch (Exception)                                                           // Here I catch all exceptions as in the end I would re-throw it
+            {
+                try                                                                     // another nested try-catch to ensure user has one more chance to enter correct string
+                {
+                    Console.WriteLine("You have entered incorrect string to convert." + $"\n" + "Please try once more to enter VALID string for convertion: ");
+                    myDouble = Double.Parse(Console.ReadLine(), System.Globalization.NumberStyles.AllowDecimalPoint);
+                    Console.WriteLine("Successfully converted to: " + $"{myDouble}");
+                    return myDouble;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Sorry, no more tries"+ $"\n");
+                    Console.WriteLine(ex.ToString());                                    // printing out exception to console
+                    throw;                                                               // rethrowing the exception
+                }
+            }
+            
+        }
+    }
+}
---
+++ ConsoleApplication1/Homework 12/HM12 Ex.2 TXT content to console/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+
+namespace HM12_Ex._2_TXT_content_to_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ShowContent();
+            Console.ReadLine();
+
+        }
+
+        /*
+        2. Write an application to show content of txt file (use File.ReadAllText()) to console. 
+        Display proper messages to user in edge scenarios (file not present, empty path)
+        */
+
+        /// <summary>
+        /// This method opens a file, reads each line of the file, and then adds each line as an element of a string. It then closes the file. 
+        /// The resulting string does not contain the terminating carriage return and/or line feed.
+        /// This method attempts to automatically detect the encoding of a file based on the presence of byte order marks.Encoding formats UTF-8 and UTF-32 can be detected.
+        /// Displays proper messages to user in edge scenarios (file not present, empty path, etc)
+        /// </summary>
+        public static void ShowContent()
+        {
+            Console.WriteLine("Please provide a path to your file with extension: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine(File.ReadAllText(path));
+            }
+
+            // The following catch blocks will catch exceptions in order from most to least specific and print out some textual information and actual error text.
+
+            catch (SecurityException e) { Console.WriteLine("\n The caller does not have the required permission.\n"); Console.WriteLine(e.ToString()); }
+            catch (NotSupportedException e) { Console.WriteLine("\n Path is in an invalid format.\n"); Console.WriteLine(e.ToString()); }
+            catch (FileNotFoundException e) { Console.WriteLine("\n The file specified in path was not found.\n"); Console.WriteLine(e.ToString()); }
+            catch (UnauthorizedAccessException e) { Console.WriteLine("\n Path specified a file that is read-only \n- or -This operation is not supported on the current platform. \n- or - path specified a directory. \n- or - The caller does not have the required permission. \n"); Console.WriteLine(e.ToString()); }
+            catch (PathTooLongException e) { Console.WriteLine("\n The specified path, file name, or both exceed the system-defined maximum length. \n For example, on Windows-based platforms, paths must be less than 248 characters, and file names must be less than 260 characters.\n"); Console.WriteLine(e.ToString()); }
+            catch (DirectoryNotFoundException e) { Console.WriteLine("\n The specified path is invalid (for example, it is on an unmapped drive). \n"); Console.WriteLine(e.ToString()); }
+            catch (IOException e) { Console.WriteLine("\n An I/O error occurred while opening the file. \n"); Console.WriteLine(e.ToString()); }
+            catch (ArgumentNullException e) { Console.WriteLine("\n Path is null. \n"); Console.WriteLine(e.ToString()); }
+            catch (ArgumentException e) { Console.WriteLine("\n Path is a zero-length string, contains only white space, or contains one or more invalid characters as defined by InvalidPathChars. \n"); Console.WriteLine(e.ToString()); }
+        }
+    }
+}
