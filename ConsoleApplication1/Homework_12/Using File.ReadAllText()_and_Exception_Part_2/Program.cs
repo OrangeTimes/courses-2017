c7e11ed1059fbcfa9e70c504fdafbfa84844f7fe
NZirchak <hereynadiia@gmail.com>
2017-08-31 16:39:17 +0300

    Homework_12

---
+++ ConsoleApplication1/Homework_12/Using File.ReadAllText()_and_Exception_Part_2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Using_File.ReadAllText_and_Exception_Part_2
+{
+	class Program
+	// Write an application to show content of txt file (use File.ReadAllText()) to console.
+	// Display proper messages to user in edge scenarios (file not present, empty path).
+	{
+		static void Main(string[] args)
+		{
+			DisplayContent();
+			Console.ReadLine();
+		}
+		public static void DisplayContent()
+		{
+			Console.WriteLine("Please specify a path to your file with extension: ");
+			string path = Console.ReadLine();
+			try
+			{
+				// if the file exist and path is correct - the text of the file will be displayed in console
+				// in other cases, one of the specified exception will be thrown
+				Console.WriteLine(File.ReadAllText(path));
+			}
+			catch (ArgumentNullException e)
+			{
+				Console.WriteLine("Error Message: path is null");
+				Console.WriteLine(e.ToString());
+			}
+			catch (ArgumentException e)
+			{
+				Console.WriteLine("Error Message: path is a zero-length string, contains only white space, or contains one or more invalid characters as defined by InvalidPathChars");
+				Console.WriteLine(e.ToString());
+			}
+			catch (UnauthorizedAccessException e)
+			{
+				Console.WriteLine("Error Message: path specified a directory or the caller does not have the required permission.");
+				Console.WriteLine(e.ToString());
+			}
+			catch (PathTooLongException e)
+			{
+				Console.WriteLine("Error Message: The specified path, file name, or both exceed the system-defined maximum length. For example, on Windows-based platforms, paths must be less than 248 characters, and file names must be less than 260 characters.");
+				Console.WriteLine(e.ToString());
+			}
+			catch (DirectoryNotFoundException e)
+			{
+				Console.WriteLine("Error Message: The specified path is invalid (for example, it is on an unmapped drive).");
+				Console.WriteLine(e.ToString());
+			}
+			catch (NotSupportedException e)
+			{
+				Console.WriteLine("Error Message: path is in an invalid format.");
+				Console.WriteLine(e.ToString());
+			}
+			catch (FileNotFoundException e)
+			{
+				Console.WriteLine("Error Message: The file specified in path was not found.");
+				Console.WriteLine(e.ToString());
+			}
+			catch (IOException e)
+			{
+				Console.WriteLine("Error Message: An I/O error occurred while opening the file. Path specified a file that is read - only or operation is not supported on the current platform.");
+				Console.WriteLine(e.ToString());
+			}
+			catch (SecurityException e)
+			{
+				Console.WriteLine("Error Message: The caller does not have the required permission.");
+				Console.WriteLine(e.ToString());
+			}
+		}
+	}
+}
---
+++ ConsoleApplication1/Homework_12/Using_Double.Parse_and_Exceptions_Part_1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Using_Double.Parse_and_Exceptions_Part_1
+// Write a program to check if a string is a valid number (double) using Double.Parse and show corresponding error to user.
+// Try to recover from this exception by asking user for valid input one more time.
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// using bool - the user gets the message till he enters an invalid value
+			bool inputValueIsValid = false;
+			do
+			{
+				Console.Write("Please specify the value: ");
+				string inputValue = Console.ReadLine();
+				try
+				{
+					//Double.Parse - converts a string representation of the number to double number
+					Double.Parse(inputValue);
+					inputValueIsValid = true;
+				}
+				catch (ArgumentNullException)
+				{
+					Console.WriteLine("Error Message: Argument null");
+				}
+				catch (FormatException)
+				{
+					Console.WriteLine("Error Message: does not represent a number in a valid format");
+				}
+				catch (OverflowException)
+				{
+					Console.WriteLine("Error Message: represents a number that is less than MinValue or greater than MaxValue");
+				}
+				catch (ArgumentException)
+				{
+					Console.WriteLine("Error Message: style is not a NumberStyles value or style includes the AllowHexSpecifier value");
+				}
+			}
+			while (inputValueIsValid == false);
+		}
+
+	}
+}
