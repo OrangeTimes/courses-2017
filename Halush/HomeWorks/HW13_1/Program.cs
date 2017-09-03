0368059c0c735584116f3d50cea3b56b51af8c66
Oleg Galush <oleggalushh@gmail.com>
2017-09-03 23:47:33 +0300

    HW13_1 Done/Added

---
+++ Halush/HomeWorks/HW13_1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Write a program that will use coordinates of 3 points entered by user and calculate area of triangle created by those 3 dots.
+Use structs or classes(Point, Triangle) in your implementation.
+*/
+
+namespace HW13_1
+{
+    //This class is for points' coordinates
+    public class Point
+    {
+        public int x;
+        public int y;
+    }
+    //This class is for 3 points of a triangle
+    public class Triangle
+    {
+        public Point a;
+        public Point b;
+        public Point c;
+        //This method calculates triangle's area
+        public double GetTriangleArea(Point a, Point b, Point c)
+        {
+            double abLenght = GetDistance(a, b);
+            double bcLenght = GetDistance(b, c);
+            double caLenght = GetDistance(c, a);
+            double sum = (abLenght + bcLenght - caLenght) * (abLenght - bcLenght + caLenght) * (-abLenght + bcLenght + caLenght) * (abLenght + bcLenght + caLenght);
+            double result = 0.25 * Math.Sqrt(sum);
+            return result;
+        }
+        //This method calculaates a side's distance
+        public double GetDistance(Point a, Point b)
+        {
+            int arg1 = a.x - b.x;
+            int arg2 = a.y - b.y;
+            double result = Math.Sqrt(Math.Pow(arg1, 2) + Math.Pow(arg2, 2));
+            return result;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create new object Trianlge and initialize 3 points of it
+            Triangle myTriangle = new Triangle();
+            myTriangle.a = new Point();
+            myTriangle.b = new Point();
+            myTriangle.c = new Point();
+            Console.WriteLine("To calculate the area of a triangle you have to enter coordiantes of 3 points.");
+            Console.WriteLine("Please enter coordinates of a first point:");
+            myTriangle.a.x = Convert.ToInt32(Console.ReadLine());
+            myTriangle.a.y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter coordinates of a second point:");
+            myTriangle.b.x = Convert.ToInt32(Console.ReadLine());
+            myTriangle.b.y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter coordinates of a third point:");
+            myTriangle.c.x = Convert.ToInt32(Console.ReadLine());
+            myTriangle.c.y = Convert.ToInt32(Console.ReadLine());
+            double area = myTriangle.GetTriangleArea(myTriangle.a, myTriangle.b, myTriangle.c);
+            Console.WriteLine("The area of your triangle is {0}", area);
+            Console.ReadKey();
+        }
+    }
+}
--- Halush/HomeWorks/testing/Program.cs
+++ Halush/HomeWorks/testing/Program.cs
@@ -6,29 +6,17 @@ using System.Threading.Tasks;
 using System.Diagnostics;
 
 
+/*
+
+*/
+
+
 namespace testing
 {
     class Program
     {
         public static void Main()
         {
-            int n = 6;
-            for (int i = 1; i <= n; i++)
-            {
-                for (int j = 1; j <= n - 1 + i; j++)
-                {
-                    if (j <= n - i)
-                    {
-                        Console.Write(" ");
-                    }
-                    if (j > n - i)
-                    {
-                        Console.Write("*");
-                    }
-                }
-                Console.WriteLine("");
-            }
-            Console.ReadKey();
         }
     }
 }
