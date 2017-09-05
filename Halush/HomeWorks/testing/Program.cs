0cced1432262bfe5475f300e7db4371432839e1f
Oleg Galush <oleggalushh@gmail.com>
2017-09-05 23:33:30 +0300

    testing

--- Halush/HomeWorks/testing/Program.cs
+++ Halush/HomeWorks/testing/Program.cs
@@ -16,12 +16,44 @@ namespace testing
 {
     public class House
     {
-
+        public void CreateHouse()
+        {
+            Door myDoor = new Door();
+            myDoor.material = "oak";
+            myDoor.height = 200;
+            myDoor.thickness = 3;
+            myDoor.width = 80;
+            Window myWindow = new Window();
+            myWindow.width = 100;
+            myWindow.height = 130;
+            myWindow.thickness = 1;
+            myWindow.trasparency = 80;
+            Wall myLeftSideWall = new LeftSideWall();
+            myLeftSideWall.material = "concrete";
+            myLeftSideWall.thickness = 20;
+            myLeftSideWall.height = 250;
+            myLeftSideWall.width = 800;
+            Wall myRightSideWall = new RightSideWall();
+            myRightSideWall.material = "concrete";
+            myRightSideWall.thickness = 20;
+            myRightSideWall.height = 250;
+            myRightSideWall.width = 800;
+            Wall myFrontWall = new FrontWall();
+            myRightSideWall.material = "concrete";
+            myRightSideWall.thickness = 20;
+            myRightSideWall.height = 250;
+            myRightSideWall.width = 500;
+            Wall myBackWall = new BackWall();
+            myRightSideWall.material = "concrete";
+            myRightSideWall.thickness = 20;
+            myRightSideWall.height = 250;
+            myRightSideWall.width = 500;
+        }
     }
 
     public class Door
     {
-        public string material;
+        public string material = "oak";
         public int height;
         public int thickness;
         public int width;
@@ -43,23 +75,19 @@ namespace testing
         public int width;
     }
 
-    public class LeftSideWall : Wall
-    { }
+    public class LeftSideWall : Wall { }
 
-    public class RightSideWall : Wall
-    { }
+    public class RightSideWall : Wall { }
 
-    public class FrontWall : Wall
-    { }
+    public class FrontWall : Wall { }
 
-    public class BackWall : Wall
-    { }
+    public class BackWall : Wall { }
 
     class Program
     {
         public static void Main()
         {
-
+            House myHouse = new House();
         }
     }
 }
