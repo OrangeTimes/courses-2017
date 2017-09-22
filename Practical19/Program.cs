eaf6c086834eab5e17bda4aed600fbd68a5cea22
Volodymyr Golub <volodymyr.golub@verint.com>
2017-09-22 10:03:41 +0300

    Added practical  #19

---
+++ Practical19/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical19
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ChildClass myChild = new ChildClass();
+			int myValue = myChild.MyLuckyNumber;
+			myChild.MyLuckyNumber = 42;
+
+		}
+	}
+
+	class ParentClass
+	{
+		private const int luckyNumber = 12;
+
+		public ParentClass()
+		{
+			// luckyNumber = 13;
+		}
+
+		public void ParentMethod()
+		{
+			
+		}
+	}
+
+	class ChildClass : ParentClass
+	{
+		private int luckyNumber = 12;
+		public void ParentMethod()
+		{
+
+		}
+		public int MyLuckyNumber
+		{
+			get
+			{
+				if (luckyNumber > 0)
+				{
+					return luckyNumber;
+				}
+				else
+				{
+					return 0;
+				}
+			}
+			set
+			{
+				luckyNumber = value;
+			}
+		}
+	
+		// Background property equivalent code
+		//public void set_LuckyNumber(int value)
+		//{
+		//	luckyNumber = value;
+		//}
+
+		//public int get_LuckyNumber(int value)
+		//{
+		//	if (luckyNumber > 0)
+		//	{
+		//		return luckyNumber;
+		//	}
+		//	else
+		//	{
+		//		return 0;
+		//	}
+		//}
+
+		public void ChildMethod()
+		{
+			base.ParentMethod();
+			// code here
+			this.ParentMethod();
+		}
+
+		public void Save()
+		{
+			ChildClassEvaluator childClassEvaluator = new ChildClassEvaluator();
+			childClassEvaluator.DoSomething(this);
+		}
+	}
+
+
+	class ChildClassEvaluator
+	{
+		public void DoSomething(ChildClass childClass)
+		{
+			childClass.ChildMethod();
+		}
+	}
+}
