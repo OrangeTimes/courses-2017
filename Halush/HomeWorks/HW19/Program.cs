a95f358a7ee2c6cdc9abd2370f9739e184547074
Oleg Galush <oleggalushh@gmail.com>
2017-09-24 21:07:33 +0300

    HW19 Done/Added

---
+++ Halush/HomeWorks/HW19/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ 1. Design a parent Car class with properties (Year of Manufacture, Name, Engine etc.). 
+ Create 3 different subclasses with different engines. 
+ Year of manufacture should be above 1990 and below 2017. 
+ Name should not exceed 20 symbols. 
+ Engine should have capacity from 500 to 3000 cc (cubic centimeters) 
+
+ 2. Create a ToString() method to output car characteristics to console 
+*/
+
+namespace HW19
+{
+    public class Car
+    {
+        private int yearOfManufacturer;
+        internal protected int YearOfManufacturer
+        {
+            get
+            {
+                return yearOfManufacturer;
+            }
+            set
+            {
+                if (value > 1989 && value < 2018)
+                    yearOfManufacturer = value;
+            }
+        }
+
+        private string name = "Name is not provided";
+        internal protected string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (value.Length > 0 && value.Length < 21)
+                    name = value;
+            }
+        }
+
+        private int engine;
+        internal protected int Engine
+        {
+            get
+            {
+                return engine;
+            }
+            set
+            {
+                if (value > 499 && value < 3001)
+                    engine = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            string myString = "Year of manufacturer is: " + yearOfManufacturer + ". " + "Engine is: " + engine + ". " + "Name is: " + name + ".";
+            return myString;
+        }
+    }
+
+    public class SUV : Car
+    {
+        public SUV(string name, int engine, int year)
+        {
+            Name = name;
+            Engine = engine;
+            YearOfManufacturer = year;
+        }
+        public SUV()
+        {
+            Name = "SUV";
+            Engine = 2000;
+            YearOfManufacturer = 2010;
+        }
+    }
+
+    public class Truck : Car
+    {
+        public Truck(string name, int engine, int year)
+        {
+            Name = name;
+            Engine = engine;
+            YearOfManufacturer = year;
+        }
+        public Truck()
+        {
+            Name = "Truck";
+            Engine = 3000;
+            YearOfManufacturer = 2010;
+        }
+    }
+
+    public class Bus : Car
+    {
+        public Bus(string name, int engine, int year)
+        {
+            Name = name;
+            Engine = engine;
+            YearOfManufacturer = year;
+        }
+        public Bus()
+        {
+            Name = "Bus";
+            Engine = 2500;
+            YearOfManufacturer = 2010;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Car newCar = new Truck();
+            Console.WriteLine(newCar.ToString());
+            Console.ReadKey();
+        }
+    }
+}
--- Halush/HomeWorks/testing/Program.cs
+++ Halush/HomeWorks/testing/Program.cs
@@ -7,16 +7,7 @@ using System.Diagnostics;
 
 
 /*
-Create an abstract class CardDeck. Create concrete class StandardCardDeck with 52 card. 
-Create class StandardCard to represent single card. 
-You need to implement an abstract method GetCard that will return you a random card from deck. 
-Try not to create an array. 
-Think of how to create card based on any given number.
-Hint: You will probably need Math.Floor, casting, and Enums, and Random. 
 
-Example of how to generate a random number between 0 (including) and 52 (excluding): 
-Random random = newRandom();
-int value = random.Next(0
 */
 
 
@@ -26,6 +17,7 @@ namespace testing
     {
         public static void Main()
         {
+
         }
     }
 }
