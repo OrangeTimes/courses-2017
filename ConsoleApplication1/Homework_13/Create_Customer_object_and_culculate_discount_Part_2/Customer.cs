ef8057c4c841492d285d8dd3464049b81f692e5c
NZirchak <hereynadiia@gmail.com>
2017-10-03 10:17:00 +0300

    Homework_13

---
+++ ConsoleApplication1/Homework_13/Create_Customer_object_and_culculate_discount_Part_2/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Create_Customer_object_and_culculate_discount_Part_2
+{
+	class Customer
+	{
+		public int CustomerAge;
+		public int customerDiscount;
+		// calculate customer discount
+		public int calculateDiscount(int CustomerAge)
+		{
+			if (CustomerAge< 30)
+			{
+				customerDiscount = 10;
+			}
+			else if (CustomerAge >= 30 && CustomerAge< 50)
+			{
+				customerDiscount = 20;
+			}
+			else
+			{
+			customerDiscount = 30;
+			}
+			return customerDiscount;
+		}
+	}
+}
---
+++ ConsoleApplication1/Homework_13/Using_struct_or_classes_calc_area_of_triangle_Part_1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Write a program that will use coordinates of 3 points entered by user and calculate area of triangle created by those 3 dots.
+// Use structs or classes(Point, Triangle) in your implementation.
+
+namespace Using_struct_or_classes_calc_area_of_triangle_Part_1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			//create pointA and assign the coordinates from the console
+			Console.WriteLine("Specify the coordinates of pointA (separate by comma): ");
+			string pointAcoordinates = Console.ReadLine();
+			Point pointA = new Point();
+			pointA.StringAsCoordinates(pointAcoordinates);
+
+			//create pointB and assign the coordinates from the console
+			Console.WriteLine("Specify the coordinates of pointB (separate by comma): ");
+			string pointBcoordinates = (Console.ReadLine());
+			Point pointB = new Point();
+			pointB.StringAsCoordinates(pointBcoordinates);
+
+			//create pointC and assign the coordinates from the console
+			Console.WriteLine("Specify the coordinates of pointC (separate by comma): ");
+			string pointCcoordinates = (Console.ReadLine());
+			Point pointC = new Point();
+			pointC.StringAsCoordinates(pointCcoordinates);
+
+			// create three sides of the triangle
+			TriangleSides sideAB = new TriangleSides();
+			sideAB.CalculateSideAB(pointA, pointB);
+
+			TriangleSides sideBC = new TriangleSides();
+			sideBC.CalculateSideBC(pointB, pointC);
+
+			TriangleSides sideAC = new TriangleSides();
+			sideAC.CalculateSideAC(pointA, pointC);
+
+			// create triangle and output the triangle area
+			Triangle myTriangle = new Triangle();
+			myTriangle.CalculateTriangleArea(sideAB, sideBC, sideAC);
+
+			Console.WriteLine($"The area of the triangle is: {myTriangle.triangleArea}");
+			Console.ReadLine();
+		}
+	}
+}
