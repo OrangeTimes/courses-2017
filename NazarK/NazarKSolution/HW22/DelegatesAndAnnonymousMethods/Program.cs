9765ad553ed6c940a392d990dc252d2573c3c6bf
Nazar Kulchytskyi <nazarkin124@gmail.com>
2017-10-25 22:39:54 +0300

    Add HW 22

---
+++ NazarK/NazarKSolution/HW22/DelegatesAndAnnonymousMethods/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndAnnonymousMethods
+{
+	class Program
+	{
+		public delegate void MathDelegate(double x, double y);
+
+		static void Main(string[] args)
+		{
+			MathDelegate myMathDelegate;
+			myMathDelegate = Add;
+			myMathDelegate += Subtract;
+			myMathDelegate += Multiply;
+			myMathDelegate += Divide;
+
+			myMathDelegate(10, 5);
+		}
+
+		public static void Add(double x, double y)
+		{
+			Console.WriteLine(x + y);
+		}
+
+		public static void Subtract(double x, double y)
+		{
+			Console.WriteLine(x - y);
+		}
+
+		public static void Multiply(double x, double y)
+		{
+			Console.WriteLine(x * y);
+		}
+
+		public static void Divide(double x, double y)
+		{
+			Console.WriteLine(x / y);
+		}
+	}
+}
