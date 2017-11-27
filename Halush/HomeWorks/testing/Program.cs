5030082d02c886a4f3db401784f2d3462b8bfac7
Oleg Galush <oleggalushh@gmail.com>
2017-11-28 00:11:35 +0200

    HW 23 Done/Added

--- Halush/HomeWorks/testing/Program.cs
+++ Halush/HomeWorks/testing/Program.cs
@@ -8,8 +8,9 @@ using System.Diagnostics;
 
 
 /*
-Create 4 anonymous methods that perform add/subtract/multiply/divide operation and log the result to console, and have a void return type. 
-Create a delegate to store all the anonymous method and later invoke it with concrete numbers
+1. Use Aggregate function to get minimum and maximum value of predefined collection of numbers
+2. Using Where method find all numbers that are divisible by 2
+3. Using Select method transform numbers to string representation with 2 number precision e.g. “2.00”(check ToString() documentation to get more info)
 */
 
 
@@ -20,7 +21,17 @@ namespace testing
     {
         public static void Main()
         {
-
+            List<double> myList = new List<double>() { 5.777, 7, 1, 4, 10.101, 33.333};
+            //Min and Max values of a collection
+            var minValueOfCollection = myList.Aggregate((start, end) => start - end);
+            var maxValueOfCollection = myList.Aggregate((start, end) => start + end);
+            //Numbers divisible by 2
+            var divisibleBy2 = myList.Where(x => x % 2 == 0).ToList();
+            //String representation with 2 numbers precision
+            var twoNumbersPrecision = myList.Select(x => x.ToString("F")).ToList();
+            Console.WriteLine("Min value of collection: {0}", minValueOfCollection);
+            Console.WriteLine("Max value of collection: {0}", maxValueOfCollection);
+            Console.ReadKey();
         }
     }
 }
