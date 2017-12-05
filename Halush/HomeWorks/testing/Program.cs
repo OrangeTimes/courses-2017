fd5a3c76560442ee10209ff0d1474d09b2632413
Oleg Galush <oleggalushh@gmail.com>
2017-12-05 23:21:13 +0200

    Added web driver

--- Halush/HomeWorks/testing/Program.cs
+++ Halush/HomeWorks/testing/Program.cs
@@ -5,13 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
-
-
-/*
-1. Use Aggregate function to get minimum and maximum value of predefined collection of numbers
-2. Using Where method find all numbers that are divisible by 2
-3. Using Select method transform numbers to string representation with 2 number precision e.g. “2.00”(check ToString() documentation to get more info)
-*/
+using OpenQA.Selenium;
 
 
 namespace testing
@@ -21,17 +15,12 @@ namespace testing
     {
         public static void Main()
         {
-            List<double> myList = new List<double>() {5.777, 7, 1, 4, 10.101, 33.333};
-            //Min and Max values of a collection
-            var minValueOfCollection = myList.Aggregate((start, end) => start < end ? start : end);
-            var maxValueOfCollection = myList.Aggregate((start, end) => start > end ? start : end);
-            //Numbers divisible by 2
-            var divisibleBy2 = myList.Where(x => x % 2 == 0).ToList();
-            //String representation with 2 numbers precision
-            var twoNumbersPrecision = myList.Select(x => x.ToString("F")).ToList();
-            Console.WriteLine("Min value of collection: {0}", minValueOfCollection);
-            Console.WriteLine("Max value of collection: {0}", maxValueOfCollection);
-            Console.ReadKey();
+            IWebDriver Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            Browser.Navigate().GoToUrl("http://google.com");
+            Browser.Manage().Window.Maximize();
+
+            IWebElement SearchField = Browser.FindElement(By.Id("lst-ib"));
+            SearchField.SendKeys("Selenium Web Driver" + Keys.Enter);
         }
     }
 }
